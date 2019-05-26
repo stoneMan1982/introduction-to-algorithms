8bf1fad5b4970b1327ee1351ea175fde1faa9151
stoneMan1982 <623421056@qq.com>
2019-05-26 17:40:32 +0800

    add merge sort

--- include/ch02/sort.hpp
+++ include/ch02/sort.hpp
@@ -29,3 +29,56 @@ void insert_sort(T &elements, CompareFunc compare) {
         elements[j + 1] = key;
     }
 }
+
+template <typename T>
+inline void copy_array(T *dest, int *src, int size) {
+    for(int i = 0;i < size;i++) {
+        dest[i] = src[i];
+    }
+}
+
+template <typename T, typename CompareFunc>
+void merge(T *array, int l, int m, int r, CompareFunc compare) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int *left = new int[n1]{0};
+    int *right = new int[n2]{0};
+    copy_array(left, array + l, n1);
+    copy_array(right, array + m + 1, n2);
+
+    int i = 0;
+    int j = 0;
+    for(int k = l;k <= r;k++) {
+        if(i >= n1 || j >= n2) {
+            for(;i < n1;i++) {
+                array[k] = left[i];
+                k++;
+            }
+            for(;j < n2;j++) {
+                array[k] = right[j];
+                k++;
+            }
+            break;
+        }
+
+        if(compare(left[i], right[j])) {
+            array[k] = left[i];
+            i++;
+        } else {
+            array[k] = right[j];
+            j++;
+        }
+    }
+
+}
+
+template <typename T, typename CompareFunc>
+void merge_sort(T *array, int begin, int end, CompareFunc compare) {
+    if(begin < end) {
+        int mid = (begin + end) >> 1;
+        merge_sort(array, begin, mid, compare);
+        merge_sort(array, mid + 1, end, compare);
+        merge(array, begin, mid, end, compare);
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -13,25 +13,7 @@ void print_array(int *array, int size) {
 
 int main() {
 
-    int array[]{3,2,7,6,5,13,10,9};
-
-    std::cout << "before sort:\n\t";
-    print_array(array, sizeof(array) / sizeof(array[0]));
-    insert_sort(array, sizeof(array) / sizeof(array[0]),[](const int &a, const int &b){ return a < b; });
-    std::cout << "after sort:\n\t";
-    print_array(array, sizeof(array) / sizeof(array[0]));
-
-    std::vector<int> arr{3,2,7,6,5,13,10,9};
-
-    typedef std::vector<int>::value_type value_type;
-    insert_sort(arr,[](value_type &a, value_type &b) {
-        return a < b;
-    });
-
-    std::cout << "after sort:\n\t";
-    for(auto iter = arr.begin();iter != arr.end();++iter) {
-        std::cout << *iter << ' ';
-    }
-    std::cout << std::endl;
+    int array[]{3,2,7,6,5,13,10,9,1,4,8};
+    merge_sort(array, 0, sizeof(array) / sizeof(int) - 1,[](int &a, int &b){ return a > b; });
     return 0;
 }
